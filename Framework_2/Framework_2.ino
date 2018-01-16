d22054111d1845a4d4d7cee3cf824e1fcf6df5f5
Rakugaki-Dan <32296527+Rakugaki-Dan@users.noreply.github.com>
2018-01-16 00:46:21 +0000

    Sorted out the framework. Might still need tweaking

--- Framework_2/Framework_2.ino
+++ Framework_2/Framework_2.ino
@@ -1,15 +1,22 @@
+#include <Password.h>
+#include <Keypad.h>
+#include <Servo.h> 
 
-#include <Password.h> //http://playground.arduino.cc/uploads/Code/Password.zip //tells to use password library
-#include <Keypad.h> //http://www.arduino.cc/playground/uploads/Code/Keypad.zip  //tells to use keypad library
-#include <Servo.h> //tells to use servo library
+Servo myServo;
+Password password = Password( "0000" ); //password to unlock, can be changed | we could make it a six digit code and have the password be "215132". B=2 O=15 M=13 B=2
 
+int successCounter = 0;
+int failCounter = 0;
 
-Servo myservo; //declares servo
-Password password = Password( "0000" ); //password to unlock, can be changed
+bool isCodeCorrect = false;
+bool isWireCut = false;
+
+const int wireToCut = A0; //set this to whatever pin you need
+int wireState = 0;
 
 const byte ROWS = 4; // Four rows
-const byte COLS = 4; // columns
-// Define the Keymap
+const byte COLS = 3; // columns
+// Defining the Keymap
 char keys[ROWS][COLS] = {
 {'1','2','3'},
 {'4','5','6'},
@@ -25,63 +32,81 @@ byte colPins[COLS] = { 5, 4, 3 };
 Keypad keypad = Keypad( makeKeymap(keys), rowPins, colPins, ROWS, COLS );
 
 void setup(){
-  Serial.begin(9600);
-  Serial.write(254);
-  Serial.write(0x01);
-  delay(200); 
-  pinMode(11, OUTPUT);  //green light
-  pinMode(12, OUTPUT);  //red light
-  myservo.attach(13); //servo on digital pin 9 //servo
-  keypad.addEventListener(keypadEvent); //add an event listener for this keypad
-  }
+  //  Serial.begin(9600);
+  //  Serial.write(254);
+  //  Serial.write(0x01);
+  //  delay(200); 
+  //  pinMode(11, OUTPUT);  //green light
+  //  pinMode(12, OUTPUT);  //red light
+    myServo.attach(13); //servo on digital pin 9 //servo
+    keypad.addEventListener(keypadEvent); //event listener for this keypad
+}
 
 void loop(){
   keypad.getKey();
-  myservo.write(0);
+  myservo.write(0); //Change this to whatever it needs to be
+  
+  if(wireToCut == LOW){
+    isWireCut = true;
+    successCounter + 3;
   }
-  //take care of some special events
-  void keypadEvent(KeypadEvent eKey){
-  switch (keypad.getState()){
-  case PRESSED:
-  
-  Serial.print("Enter:");
-  Serial.println(eKey);
-  delay(10);
-  
-  Serial.write(254);
-  
-  switch (eKey){
-    case '*': checkPassword(); delay(1); break;
+
+  if(successCounter = 4 && isCorrectCode = true && isWireCut = true){
+
+    //Put in code for win state or whatever here
+  }
+
+  if(failCounter => 3){
+
+    //put fail state here
     
-    case '#': password.reset(); delay(1); break;
-    
-     default: password.append(eKey); delay(1);
-}
-}
-}
-void checkPassword(){
-  
-if (password.evaluate()){  //if password is right open
-    
-    Serial.println("Accepted");
-    Serial.write(254);delay(10);
-    //Add code to run if it works
-    myservo.write(150); //deg
-    
-        digitalWrite(11, HIGH);//turn on
-    delay(5000); //wait 5 seconds
-    digitalWrite(11, LOW);// turn off
-    
-    
-}else{
-    Serial.println("Denied"); //if passwords wrong keep locked
-    Serial.write(254);delay(10);
-    //add code to run if it did not work
-    myservo.write(0);
-    digitalWrite(12, HIGH); //turn on
-    delay(500); //wait 5 seconds
-    digitalWrite(12, LOW);//turn off
-    
-}
+  }
 }
 
+
+  //taking care of some special events
+  void keypadEvent(KeypadEvent eKey){
+    switch (keypad.getState()){
+  //  case PRESSED:
+  //  
+  //  Serial.print("Enter:");
+  //  Serial.println(eKey);
+  //  delay(10);
+  //  
+  //  Serial.write(254);
+    
+      switch (eKey){
+        case '*': checkPassword(); delay(1); break; //* checks the password
+        
+        case '#': password.reset(); delay(1); break; //# resets the password allowing to start again
+        
+        default: password.append(eKey); delay(1);
+      }
+  }
+}
+
+
+void checkPassword(){
+  
+  if (password.evaluate()){  //if password is right, open
+  
+      //Serial.println("Accepted");
+      //Serial.write(254);delay(10);
+      //Add code to run if it works
+      isCorrectCode = true;
+      successCounter = 1;
+      
+      myservo.write(150); //change as see fit
+      
+      
+  }else{
+
+      //Serial.println("Denied"); //if passwords wrong, keep locked
+      //Serial.write(254);delay(10);
+      //add code to run if it did not work
+
+      failCounter++;
+      myservo.write(0); //change as see fit
+    
+  }
+}
